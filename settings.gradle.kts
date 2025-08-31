26241bbca3f1229b93b470da56ea99e4e1c04d70
IThundxr <me@ithundxr.dev>
2025-08-31 16:43:42 -0400

    fix proj name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-rootProject.name = "ponder"
+rootProject.name = "Ponder"
 include("common")
 include("forge")
 include("fabric")
